29eb75449b87bce42005c6ee446519b00e9a0a18
akash-singh8 <developer.akash8@gmail.com>
2023-04-28 18:52:29 +0530

    Added logic to create idea

--- Controllers/ideas-controller.js
+++ Controllers/ideas-controller.js
@@ -6,6 +6,16 @@ module.exports.fetchAllIdea = (req, res) => {
 };
 
 // Create new idea
+// Request body is where we get data which's shared while making a POST request
+module.exports.createIdea = (req, res) => {
+  const idea = req.body;
+  const currId = ideas.length;
+
+  ideas.push({ id: currId + 1, ...idea });
+
+  // Return the response, with status 201 -> means the response has been successfully created
+  res.status(201).send(idea);
+};
 
 // Update idea
 
