6b6da43572ea523c6eb8ec97bf54d439234cb88a
akash-singh8 <developer.akash8@gmail.com>
2023-04-28 18:55:38 +0530

    Added middleware to parse json data into JS objects

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 app.use(middleware);
 
 // Middleware function where we can perform checks before actual processing
