c21272b9121ed93b0c84d63ee6da77a1523eac2c
akash-singh8 <developer.akash8@gmail.com>
2023-04-28 16:53:01 +0530

    Added main server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+app.use(middleware);
+
+// Middleware function where we can perform checks before actual processing
+function middleware(req, res, next) {
+  console.log("Middleware code logic!");
+  next(); // It'll pass on the control to the next step
+}
+
+// Connect routes to the server
+require("./Routes/ideas-route")(app);
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
