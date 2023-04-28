1fa3bb419986bf3d2e7b380469226395db8e6d1e
akash-singh8 <developer.akash8@gmail.com>
2023-04-28 18:53:24 +0530

    Added route for post requests

--- Routes/ideas-route.js
+++ Routes/ideas-route.js
@@ -3,4 +3,7 @@ const ideaController = require("../Controllers/ideas-controller");
 module.exports = (app) => {
   // Listening for get request
   app.get("/ideaApp/v1/ideas", ideaController.fetchAllIdea);
+
+  // Listening for post request
+  app.post("/ideaApp/v1/ideas", ideaController.createIdea);
 };
