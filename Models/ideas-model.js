1f69339d68fbd84e2d7896056a828dd50183671d
akash-singh8 <developer.akash8@gmail.com>
2023-04-28 16:19:42 +0530

    Added models

---
+++ Models/ideas-model.js
@@ -0,0 +1,10 @@
+// Currently we'll be using javascript objects as our database but in actual project we can set real database.
+
+module.exports = {
+  1: {
+    id: 1,
+    idea_name: "Ideas API",
+    author_name: "Akash Singh",
+    idea_description: "API to share ideas with across different users",
+  },
+};
